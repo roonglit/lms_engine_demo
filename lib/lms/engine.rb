d40693654c81c19ed998573dd76b7d1e958f922d
Roonglit Chareonsupkul <roonglit@gmail.com>
2025-09-21 21:15:39 +0700

    build tailwindcss with daisyui plugin

--- lib/lms/engine.rb
+++ lib/lms/engine.rb
@@ -1,5 +1,15 @@
 module Lms
   class Engine < ::Rails::Engine
     isolate_namespace Lms
+
+    # Add engine's assets to loading path
+    # config.before_configuration do
+    #   config.assets.paths << root.join("app/assets/builds")
+    # end
+
+    # # Add engine's built CSS to precompile list
+    # initializer "lms.assets" do |app|
+    #   app.config.assets.precompile += %w[lms/admin/tailwind.css]
+    # end
   end
 end
