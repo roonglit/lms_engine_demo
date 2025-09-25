939e4fcd4d9bee484ecb70097181b1f98d251410
Roonglit <roonglit@gmail.com>
2025-09-25 18:07:59 +0700

    remove unused line

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,5 @@
 # Pin the controllers index
 pin "lms/controllers", to: "lms/controllers/index.js"
 
-# Pin all controllers from the engine
-pin_all_from "app/javascript/lms/controllers", under: "lms/controllers"
-
 # Explicitly pin individual controllers
 pin "lms/controllers/course_controller", to: "lms/controllers/course_controller.js"
