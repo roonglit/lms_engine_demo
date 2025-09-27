ecc202b3977776c3b30659b14c0eeab7348a0e18
Roonglit Chareonsupkul <roonglit@gmail.com>
2025-09-27 14:52:12 +0700

    add dependent destroy to course and section

--- app/models/lms/course.rb
+++ app/models/lms/course.rb
@@ -3,7 +3,7 @@ module Lms
     has_rich_text :content
     has_one_attached :cover
 
-    has_many :sections
+    has_many :sections, dependent: :destroy
     accepts_nested_attributes_for :sections, allow_destroy: true
   end
 end
--- app/models/lms/section.rb
+++ app/models/lms/section.rb
@@ -2,7 +2,7 @@ module Lms
   class Section < ApplicationRecord
     belongs_to :course
 
-    has_many :curriculum_items
+    has_many :curriculum_items, dependent: :destroy
     accepts_nested_attributes_for :curriculum_items, allow_destroy: true
   end
 end
