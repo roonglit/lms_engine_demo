a22a3279f844297214d2816ff941b2a5a95d3332
Roonglit Chareonsupkul <roonglit@gmail.com>
2025-10-02 15:39:09 +0700

    add admin for other content type

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Lms::Engine.routes.draw do
-  resources :events
-  resources :videos
 	constraints(->(req) { req.path.include?('admin')}) do
 		scope module: 'admin', as: :admin do
+			resources :articles
+			resources :videos
+			resources :events
 			resources :courses
 		end
 	end
 
 	resources :posts, only: %i[new]
 	resources :articles
+	resources :events
+  resources :videos
   resources :courses, only: %i[index show]
 
 	root to: "home#index"
