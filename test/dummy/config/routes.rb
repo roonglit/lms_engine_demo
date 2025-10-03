e37e799b69a2459813771127334d21e53096b628
Roonglit Chareonsupkul <roonglit@gmail.com>
2025-10-03 12:14:12 +0700

    fix content path

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
   devise_for :users
   mount Lms::Engine => "/lms"
 
