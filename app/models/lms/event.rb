ae36f391b6658b9859d744c366ed82b7ab2daffc
Roonglit <roonglit@gmail.com>
2025-10-08 13:15:41 +0700

    fix admin event section

--- app/models/lms/event.rb
+++ app/models/lms/event.rb
@@ -3,5 +3,6 @@ module Lms
     has_one :content, as: :contentable, dependent: :destroy
 
     accepts_nested_attributes_for :content, allow_destroy: true
+    delegate :title, :subtitle, :description, :cover, to: :content, allow_nil: true
   end
 end
