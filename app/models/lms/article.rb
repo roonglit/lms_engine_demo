99730cdf2371775d92ff42adc92e711ce0b75760
Roonglit Chareonsupkul <roonglit@gmail.com>
2025-10-21 20:31:30 +0700

    emit an event when an article is created

--- app/models/lms/article.rb
+++ app/models/lms/article.rb
@@ -4,5 +4,13 @@ module Lms
     accepts_nested_attributes_for :content, allow_destroy: true
 
     delegate :title, :subtitle, :description, :cover, to: :content, allow_nil: true
+
+    after_create :notify_article_created
+
+    private
+
+    def notify_article_created
+      ActiveSupport::Notifications.instrument("lms.article.created", article_id: id)
+    end
   end
 end
